a5bf6ddbe7cdb342907ff79dad1829639160998e
Samantha Ming <samantham@iqmetrix.com>
2016-11-04 12:25:50 -0700

    Navigation

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.set('views', './src/views');
 app.set('view engine', '.ejs');
 
 app.get('/', function (req, res) {
-    res.render('index', { title:'Hello from render', list: ['a','b']});
+    res.render('index', { 
+        title:'Hello from render', 
+        nav: [
+                {Link:'/Books', Text:'Books'},  
+                {Link:'/Authors', Text:'Authors'}  
+        ]
+    });
 });
 
 app.get('/books', function (req, res) {
