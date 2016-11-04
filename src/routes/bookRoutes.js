55057923b4e843312605ac9442151f545f3fdaea
Samantha Ming <samantham@iqmetrix.com>
2016-11-04 16:54:35 -0700

    Controllers

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,41 +3,15 @@ var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 
+
 var router = function (nav) {
+  var bookController = require('../controllers/bookController')(null, nav);
 
   bookRouter.route('/')
-    .get(function (req, res) {
-
-      var url = 'mongodb://localhost:27017/libraryApp';
-      mongodb.connect(url, function (err, db) {
-        var collection = db.collection('books');
-        collection.find({}).toArray(function (err, results) {
-          res.render('bookListView', {
-            title: 'Books',
-            nav: nav,
-            books: results
-          });
-        });
-      });
-      
-    });
+    .get(bookController.getIndex);
 
   bookRouter.route('/:id')
-    .get(function (req, res) {
-      var id = new objectId(req.params.id);
-      var url = 'mongodb://localhost:27017/libraryApp';
-
-      mongodb.connect(url, function (err, db) {
-        var collection = db.collection('books');
-        collection.findOne({_id: id}, function (err, results) {
-          res.render('bookView', {
-            title: 'Books',
-            nav: nav,
-            book: results
-          });
-        });
-      });
-    });
+    .get(bookController.getById);
 
   return bookRouter;
 };
