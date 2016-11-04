0b481b8f8eca7e3a6f9d3dc6b0eaf06a6196a094
Samantha Ming <samantham@iqmetrix.com>
2016-11-03 17:47:05 -0700

    Simple Routing

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 var port = 5000;
 
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.get('/books', function (req, res) {
+  res.send('Hello Books');
+});
+
 app.listen(port, function (err) {
   console.log('running server on port: ' + port);
 });
