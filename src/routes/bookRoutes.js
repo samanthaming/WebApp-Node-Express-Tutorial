23b078dbfbabe36a0485afd8f99b60cf064e6e91
Samantha Ming <samantham@iqmetrix.com>
2016-11-04 13:14:32 -0700

    Rendering a Single Book

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,79 +1,77 @@
 var express = require('express');
 var bookRouter = express.Router();
 
-var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-        },
-    {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: false
-        },
-    {
-        title: 'The Time Machine',
-        genre: 'Science Fiction',
-        author: 'H. G. Wells',
-        read: false
-        },
-    {
-        title: 'A Journey into the Center of the Earth',
-        genre: 'Science Fiction',
-        author: 'Jules Verne',
-        read: false
-        },
-    {
-        title: 'The Dark World',
-        genre: 'Fantasy',
-        author: 'Henry Kuttner',
-        read: false
-        },
-    {
-        title: 'The Wind in the Willows',
-        genre: 'Fantasy',
-        author: 'Kenneth Grahame',
-        read: false
-        },
-    {
-        title: 'Life On The Mississippi',
-        genre: 'History',
-        author: 'Mark Twain',
-        read: false
-        },
-    {
-        title: 'Childhood',
-        genre: 'Biography',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-        }
-];
+var books = [{
+  title: 'War and Peace',
+  genre: 'Historical Fiction',
+  author: 'Lev Nikolayevich Tolstoy',
+  read: false
+}, {
+  title: 'Les Misérables',
+  genre: 'Historical Fiction',
+  author: 'Victor Hugo',
+  read: false
+}, {
+  title: 'The Time Machine',
+  genre: 'Science Fiction',
+  author: 'H. G. Wells',
+  read: false
+}, {
+  title: 'A Journey into the Center of the Earth',
+  genre: 'Science Fiction',
+  author: 'Jules Verne',
+  read: false
+}, {
+  title: 'The Dark World',
+  genre: 'Fantasy',
+  author: 'Henry Kuttner',
+  read: false
+}, {
+  title: 'The Wind in the Willows',
+  genre: 'Fantasy',
+  author: 'Kenneth Grahame',
+  read: false
+}, {
+  title: 'Life On The Mississippi',
+  genre: 'History',
+  author: 'Mark Twain',
+  read: false
+}, {
+  title: 'Childhood',
+  genre: 'Biography',
+  author: 'Lev Nikolayevich Tolstoy',
+  read: false
+}];
 
 bookRouter.route('/')
-    .get(function(req, res) {
-        res.render('books', { 
-        title:'Books', 
-        nav: [
-                {Link:'/Books', Text:'Books'},  
-                {Link:'/Authors', Text:'Authors'}  
-        ],
-        books
-      });
+  .get(function (req, res) {
+    res.render('bookListView', {
+      title: 'Books',
+      nav: [{
+        Link: '/Books',
+        Text: 'Books'
+      }, {
+        Link: '/Authors',
+        Text: 'Authors'
+      }],
+      books
+    });
   });
-    
+
 bookRouter.route('/:id')
   .get(function (req, res) {
     var id = req.params.id;
-    res.render('book', {
+    res.render('bookView', {
       title: 'Books',
-      nav: [
-        { Link: '/Books', Text: 'Books' },
-        { Link: '/Authors', Text: 'Authors' }
-      ],
+      nav: [{
+        Link: '/Books',
+        Text: 'Books'
+      }, {
+        Link: '/Authors',
+        Text: 'Authors'
+      }],
       book: books[id]
     });
-        
-module.exports = bookRouter;     
+  });
+
+module.exports = bookRouter;
